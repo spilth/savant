bf1d25a7fd1df40b86dbe81e2b33b21fa5926bdb
Brian Kelly <polymonic@gmail.com>
2017-11-06 16:28:31 -0500

    Clean up search command parameters

--- src/main/kotlin/org/spilth/search/SearchCommand.kt
+++ src/main/kotlin/org/spilth/search/SearchCommand.kt
@@ -6,15 +6,12 @@ import org.spilth.validators.FormatValidator
 
 @Parameters(commandNames = arrayOf("search"), commandDescription = "Searches Maven Central for dependencies")
 class SearchCommand {
-    @Parameter(description = "Search term", arity = 1)
-    var searchTerms: List<String>? = null
+    @Parameter(description = "Search Term", required = true)
+    var searchTerm: String? = null
 
-    @Parameter(names = arrayOf("--format", "-f"), description = "Dependency output format", validateWith = arrayOf(FormatValidator::class))
+    @Parameter(names = arrayOf("--format", "-f"), description = "Dependency output format: maven (default) or gradle", validateWith = arrayOf(FormatValidator::class))
     var format = "maven"
 
     @Parameter(names = arrayOf("--help", "-h"), description = "Show help", help = true)
     var isHelp = false
-
-    val searchTerm: String
-        get() = searchTerms!![0]
 }
